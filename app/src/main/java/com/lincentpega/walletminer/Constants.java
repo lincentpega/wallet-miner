e7902feb97f9a9b956de6f1958a0e97cf99b2928
lincentpega <imkrasnyukov@gmail.com>
2022-10-05 01:57:25 +0300

    addresses scraping added

---
+++ app/src/main/java/com/lincentpega/walletminer/Constants.java
@@ -0,0 +1,6 @@
+package com.lincentpega.walletminer;
+
+public final class Constants {
+	public static final String userAgent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.2 (KHTML, like Gecko)"
+			+ " Chrome/22.0.1216.0 Safari/537.2";
+}
